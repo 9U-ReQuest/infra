586b35a6bb15faeacd78cc9f1423f336e8a030ba
EATSTEAK <me@eatsteak.dev>
2024-11-20 01:04:05 +0900

    feat(codedeploy): create `bucket` output

--- codedeploy/outputs.tf
+++ codedeploy/outputs.tf
@@ -3,7 +3,7 @@ output "bucket_arn" {
   value       = aws_s3_bucket.codedeploy.arn
 }
 
-output "bucket_arn" {
-  description = "arn of codedeploy s3 bucket"
-  value       = aws_s3_bucket.codedeploy.arn
+output "bucket" {
+  description = "name of codedeploy s3 bucket"
+  value       = aws_s3_bucket.codedeploy.bucket
 }
