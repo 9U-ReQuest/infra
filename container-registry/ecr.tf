9aa9bf903850b55a2c1a9ee8cf7f3aa34afd4a57
EATSTEAK <me@eatsteak.dev>
2024-11-20 00:53:01 +0900

    feat(container-registry): change registry name and add outputs for cd

--- container-registry/ecr.tf
+++ container-registry/ecr.tf
@@ -1,6 +1,6 @@
 # ECR 리포지토리 생성
 resource "aws_ecr_repository" "container_registry" {
-  name = "request-${terraform.workspace}.ecr"
+  name = "request-${terraform.workspace}"
 
   tags = {
     Name = "request-${terraform.workspace}.ecr"
--- container-registry/outputs.tf
+++ container-registry/outputs.tf
@@ -1,5 +1,15 @@
 
-output "ecr_id" {
-  description = "id of the container registry"
+output "id" {
+  description = "id of the container registry resource"
   value = aws_ecr_repository.container_registry.id
+}
+
+output "registry_id" {
+  description = "id of the container registry"
+  value = aws_ecr_repository.container_registry.registry_id
+}
+
+output "repository_url" {
+  description = "url of repository"
+  value = aws_ecr_repository.container_registry.repository_url
 }
